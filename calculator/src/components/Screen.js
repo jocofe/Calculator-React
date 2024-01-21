748041e052288a70eb704552955ab720aa0f817b
José <helloyinx@gmail.com>
2024-01-21 18:44:48 +0100

    Creating conts and functions for getting work the calculator, also change exports and imports to named syntax.

--- calculator/src/components/Screen.js
+++ calculator/src/components/Screen.js
@@ -1,12 +1,10 @@
 import { Textfit } from "react-textfit";
 import "./Screen.css";
 
-const Screen = ({ value }) => {
+export const Screen = ({ value }) => {
   return (
     <Textfit className="screen" mode="single" max={70}>
       {value}
     </Textfit>
   );
 };
-
-export default Screen;
--- calculator/src/components/Wrapper.js
+++ calculator/src/components/Wrapper.js
@@ -1,7 +1,6 @@
 import "./Wrapper.css";
 
-const Wrapper = ({ children }) => {
+export const Wrapper = ({ children }) => {
   return <div className="wrapper">{children}</div>;
 };
 
-export default Wrapper;
